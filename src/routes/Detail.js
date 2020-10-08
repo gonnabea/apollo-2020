423737983a843e1e5e43e33cc0bef93b27394039
Choi Jiwon <winning1993@naver.com>
2020-10-09 08:39:41 +0900

    #1.2 GET MOVIE Query

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,30 @@
-export default () => "Detail"
+import React from "react"
+import { useParams } from "react-router-dom"
+import { gql } from "apollo-boost"
+import { useQuery } from "@apollo/react-hooks"
+
+const GET_MOVIE = gql`
+  query movie($id: Int!) {
+    #   아폴로 전용 문법: 타입체크
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_full
+    }
+  }
+`
+
+export default () => {
+  const { id } = useParams()
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id: parseInt(id) },
+  })
+
+  if (loading) {
+    return "loading"
+  }
+  if (data && data.movie) {
+    return data.movie[0].title
+  }
+}
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,52 @@
 import React from "react"
 import { gql } from "apollo-boost"
 import { useQuery } from "@apollo/react-hooks"
+import styled from "styled-components"
+import Movie from "../components/Movie"
+
+const Loader = styled.div`
+  width: 100vw;
+  height: 100vh;
+  font-size: 30px;
+  color: white;
+  box-shadow: 0 0 10px black;
+  background-color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Body = styled.div`
+  width: 100vw;
+  height: 100vh;
+`
+
+const Header = styled.header`
+  height: 40%;
+  background-color: #e7586a;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const HeaderTitle = styled.h1`
+  color: white;
+  font-size: 30px;
+  margin-bottom: 10px;
+  font-weight: 700;
+`
+const HeaderDescription = styled.h3`
+  color: rgba(255, 255, 255, 0.8);
+  font-size: 16px;
+`
+
+const Main = styled.main`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  height: 60%;
+`
 
 const GET_MOVIES = gql`
   {
@@ -14,5 +60,19 @@ const GET_MOVIES = gql`
 export default () => {
   const { loading, error, data } = useQuery(GET_MOVIES)
   console.log(loading, error, data)
-  return loading ? <h2>로딩중...</h2> : data.movies.map((movie) => <h2>{movie.id}</h2>)
+  return loading ? (
+    <Loader>로딩중...</Loader>
+  ) : (
+    <Body>
+      <Header>
+        <HeaderTitle>Apollo 2020</HeaderTitle>
+        <HeaderDescription>어썸한 그래쁘뀨엘!</HeaderDescription>
+      </Header>
+      <Main>
+        {data.movies.map((movie) => (
+          <Movie id={movie.id} />
+        ))}
+      </Main>
+    </Body>
+  )
 }
