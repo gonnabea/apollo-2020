01d772ad95cbb7fee8c003b4581b1a686d826077
Choi Jiwon <winning1993@naver.com>
2020-10-11 09:02:36 +0900

    #2.1 Local State part Two

--- src/apollo.js
+++ src/apollo.js
@@ -5,6 +5,19 @@ const client = new ApolloClient({
   resolvers:{
     Movie:{
       isLiked: () => false
+    },
+    Mutation: {
+      likeMovie: (_, {id}, {cache}) => {
+        cache.writeData({id: `Movie:${id}`, data:{
+          isLiked: true,
+          // medium_cover_image: "Dffsdasfds" <- 기존 백엔드에서 온 데이터도 이런식으로 변형 가능: 어쌈!!
+        }})
+      },
+      // unLikeMovie: (_,{id}, {cache}) => {
+      //   cache.writeData({id: `Movie:${id}`, data: {
+      //     isLiked: false
+      //   }})
+      // }
     }
   }
 })
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,6 +1,17 @@
 import React from "react"
 import styled from "styled-components"
 import { Link } from "react-router-dom"
+import { gql } from "apollo-boost"
+import { useMutation } from "@apollo/react-hooks"
+
+const LIKE_MOVIE = gql`
+mutation likeMovie($id: Int!){
+  likeMovie(id:$id) @client
+}
+# mutation unLikeMovie($id: Int!){
+#   unLikeMovie(id:$id) @client
+# }
+`
 
 const Poster = styled.img`
 width: 300px;
@@ -13,18 +24,19 @@ const Title = styled.h1`
   margin-bottom: 10px;
 `
 
-const handleLike =() => {
-  
-}
 
-export default ({ id,title,imgSrc,isLiked }) => (
+export default ({ id,title,imgSrc,isLiked }) => {
+  const [likeMovie] = useMutation(LIKE_MOVIE, {variables: {id: parseInt(id)}})
+  // const [unLikeMovie] = useMutation(LIKE_MOVIE, {variables: {id: parseInt(id)}})
+
+  return (
   <div>
     <Link to={`/${id}`}>
       <Poster src={imgSrc} />
       <Title>
       {title}
         </Title></Link>
-        <button onClick={handleLike}>{isLiked ? "unLike" : "Like"}</button>
-      {isLiked.toString()}
+        <button onClick={isLiked? null: likeMovie}>{isLiked ? "unLike" : "Like"}</button>
   </div>
 )
+}
